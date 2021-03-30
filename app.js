ad3c5a667bc633e48174fdf8cfb4028ef39f2016
Rich Browne <richard.browne@openfin.co>
2021-03-30 14:53:19 +0100

    Updated manifest URLs to point to CDN. Added "prod" flag to run demo app locally against CDN URLs.

--- app.js
+++ app.js
@@ -11,16 +11,19 @@ const runtimeVersion = '19.89.57.15';
     var app = express();
     var port = process.env.PORT || 1337;
     var siteRoot = `http://localhost:${port}/`;
+    var isProduction = process.argv.includes('prod');
 
     app.set('port', port);
 
     // Modify manifest file contents for local debugging
     app.use('/bloomberg-service/demo/app.json', (req, res) => {
         var appConfig = require('./demo/app.json');
-
-        appConfig.runtime.version = runtimeVersion;
-        appConfig.startup_app.url = url.resolve(siteRoot, url.parse(appConfig.startup_app.url).path);
-        appConfig.services[0].manifestUrl = url.resolve(siteRoot, url.parse(appConfig.services[0].manifestUrl).path);
+        appConfig.startup_app.url = url.resolve(siteRoot, appConfig.startup_app.url);
+        
+        if (!isProduction) {
+            appConfig.runtime.version = runtimeVersion;            
+            appConfig.services[0].manifestUrl = url.resolve(siteRoot, '/bloomberg-service/provider/app.json');
+        }
 
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(appConfig, null, 2));
@@ -29,10 +32,12 @@ const runtimeVersion = '19.89.57.15';
     app.use('/bloomberg-service/provider/app.json', (req, res) => {
         var appConfig = require('./provider/app.json');
 
-        appConfig.startup_app.autoShow = true;
-        appConfig.startup_app.url = url.resolve(siteRoot, url.parse(appConfig.startup_app.url).path);
-        appConfig.appAssets[0].src = url.resolve(siteRoot, url.parse(appConfig.appAssets[0].src).path);
-        appConfig.appAssets[0].args = `--debug${(process.argv.includes('mock') ? ' --mock' : '')}`;
+        if (!isProduction) {
+            appConfig.startup_app.autoShow = true;
+            appConfig.startup_app.url = url.resolve(siteRoot, '/bloomberg-service/provider/index.html');
+            appConfig.appAssets[0].src = url.resolve(siteRoot, '/bloomberg-service/provider/assets/OpenFin.BloombergService.zip');
+            appConfig.appAssets[0].args = `--debug${(process.argv.includes('mock') ? ' --mock' : '')}`;
+        }
 
         res.setHeader('Content-Type', 'application/json');
         res.end(JSON.stringify(appConfig, null, 2));
